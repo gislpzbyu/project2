d5db4b712767f749b57e54920fe7d8dd7f25a20f
gislpzbyu <64442761+gislpzbyu@users.noreply.github.com>
2024-02-09 01:58:36 -0600

    Add files via upload
    done

---
+++ main.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./data/database');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+
+        app.listen(port, () => {
+            console.log(`Running on port ${port}`)
+        });
+    }
+});
